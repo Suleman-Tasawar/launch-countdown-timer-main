75f84722d9439f1dc9017191810dbf2311e4e0c7
Suleman-Tasawar <sulemantasawar9@gmail.com>
2023-07-25 01:26:44 -0700

    Referacted the Code

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
-//State Variables
-let seconds, minutes, hours, days, countDownInterval;
+// Countdown timer settings
+const initialDays = 8;
+const initialHours = 23;
+const initialMinutes = 55;
+const initialSeconds = 48;
 
-//DOM Elements
-let timeDays = document.getElementById("time--days");
-let timeHours = document.getElementById("time--hours");
-let timeMinutes = document.getElementById("time--minutes");
-let timeSeconds = document.getElementById("time--seconds");
+// DOM elements
+const timeDaysElement = document.getElementById("time--days");
+const timeHoursElement = document.getElementById("time--hours");
+const timeMinutesElement = document.getElementById("time--minutes");
+const timeSecondsElement = document.getElementById("time--seconds");
 
-function updateSeconds() {
-  let seconds = parseInt(timeSeconds.innerHTML);
+// Countdown state variables
+let days = initialDays;
+let hours = initialHours;
+let minutes = initialMinutes;
+let seconds = initialSeconds;
+let timerShouldBeRunning = true;
+
+function updateCountdown() {
   seconds--;
+
   if (seconds < 0) {
-    minutes = parseInt(timeMinutes.innerHTML);
     minutes--;
-    timeMinutes.innerHTML = minutes;
+    seconds = 59;
     if (minutes < 0) {
-      hours = parseInt(timeHours.innerHTML);
       hours--;
-      timeHours.innerHTML = hours;
+      minutes = 59;
       if (hours < 0) {
-        days = parseInt(timeDays.innerHTML);
         days--;
-        timeDays.innerHTML = days;
-        //If totalDays less than 0 the time will stop
+        hours = 23;
         if (days < 0) {
+          timerShouldBeRunning = false;
           clearInterval(countDownInterval);
-        } else {
-          timeDays.innerHTML = 8;
-          timeMinutes.innerHTML = 55;
-          timeSeconds.innerHTML = 48;
         }
-      } else {
-        timeMinutes.innerHTML = 55;
-        timeSeconds.innerHTML = 48;
       }
-    } else {
-      timeSeconds.innerHTML = 48;
     }
-  } else {
-    timeSeconds.innerHTML = seconds;
   }
+
+  // Update the DOM elements with the latest countdown values
+  timeDaysElement.innerHTML = days;
+  timeHoursElement.innerHTML = hours < 10 ? "0" + hours : hours;
+  timeMinutesElement.innerHTML = minutes < 10 ? "0" + minutes : minutes;
+  timeSecondsElement.innerHTML = seconds < 10 ? "0" + seconds : seconds;
 }
 
-countDownInterval = setInterval(updateSeconds, 1000);
+// Call the updateCountdown function every second
+const countDownInterval = setInterval(updateCountdown, 1000);
